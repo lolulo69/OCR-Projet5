687e881a82bcc1ec698ec49cddf3f02d3ba09a58
lolulo69 <lefortlucas1@gmail.com>
2022-11-03 14:16:47 +0100

    Création du script.js lié à l'index.html Création du fichier javascript de la page d'accueil, qui sert essentiellement à ajouter dynamiquement les différents produits sur la page, tout en créant le lien vers leur page produit respective.

---
+++ front/js/script.js
@@ -0,0 +1,40 @@
+//Récupération du tableau des produits via requête GET
+fetch("http://localhost:3000/api/products")
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function(products) {
+        let section = document.getElementById("items");
+
+//Boucle for qui assigne chaque produit à ses différents élément html
+        for(let product of products) {
+
+            let productLink = document.createElement("a");
+            section.appendChild(productLink);
+            productLink.setAttribute("href", "./product.html?id="+product._id);
+
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+            let productImage = document.createElement("img");
+            productArticle.appendChild(productImage);
+            productImage.setAttribute("src", product.imageUrl);
+            productImage.setAttribute("alt", product.altTxt);
+
+            let productTitle = document.createElement("h3");
+            productArticle.appendChild(productTitle);
+            productTitle.innerText = product.name;
+            productTitle.classList.add("productName");
+
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.innerText = product.description;
+            productDescription.classList.add("productDescription");
+
+        }
+    })
+    .catch((err) => {
+        console.log("Une erreur est survenue"); 
+      });
