a2bae7fe91b4473bac90e9f302cf492afc67e5e0
lolulo69 <lefortlucas1@gmail.com>
2022-11-16 15:33:31 +0100

    Création des éléments permettant l'ajout au panier Plusieurs fonctions et event listeners ont été ajoutés afin de permettre l'ajout du produit au panier en prenant en compte les paramètres de couleur et quantité. Des commentaires ont aussi été ajoutés afin de clarifier le fonctionnement du code.

--- front/js/product.js
+++ front/js/product.js
@@ -8,7 +8,10 @@ fetch("http://localhost:3000/api/products/"+ id)
             return res.json();
         }
     })
+//Ajout dynamique des différents paramètres dans les balises HTML
     .then(function(product) {
+
+        document.title = product.name;
 
         let imageContainer = document.getElementsByClassName("item__img");
         let image = document.createElement("img");
@@ -29,6 +32,7 @@ fetch("http://localhost:3000/api/products/"+ id)
         description.innerText = product.description;
 
 
+        //Boucle for of qui crée les différents choix de couleur en fonction du résultat du GET
         for(let i of product.colors) {
 
             let colorsContainer = document.getElementById("colors");
@@ -40,7 +44,64 @@ fetch("http://localhost:3000/api/products/"+ id)
         }
 
     })
+    
 //Affichage d'un message d'erreur en cas de problème à l'exécution
     .catch((err) => {
         console.log("Une erreur est survenue"); 
       });
+
+
+//Déclaration des variables utilisées pour l'ajout au panier
+let quantityElement = document.getElementById("quantity")
+let colorElement = document.getElementById("colors")
+let basketButton = document.getElementById("addToCart");
+let quantity;
+let selectedColor;
+let alreadyAdded;
+
+
+//On ajoute les 3 event listener qui vont tracker respectivement : la quantité, la couleur, et le bouton d'ajout au panier
+quantityElement.addEventListener("change", storeQuantity);
+colorElement.addEventListener("change", storeColor);
+basketButton.addEventListener("click", addToBasket);
+
+
+//Fonction qui met à jour la variable qui stock la quantité sélectionnée par l'utilisateur
+function storeQuantity(value) {
+    quantity = value.target.value;
+}
+
+//Fonction qui met à jour la variable qui stock la couleur sélectionnée par l'utilisateur
+function storeColor(value) {
+    selectedColor = value.target.value;
+}
+
+//Fonction qui gère l'ajout au panier en reprenant les différents paramètres, et met à jour l'array, avant de tout envoyer dans le DOM
+function addToBasket(event) {
+    event.stopPropagation();
+
+    let basket = JSON.parse(localStorage.getItem("basket") || "[]");
+
+    let order = {
+        id: id, 
+        quantity: quantity,
+        color: selectedColor
+    };
+    for(let element of basket) {
+        if(element.id == id && element.color == selectedColor) {
+            alreadyAdded = true;
+            let total = quantity*1+element.quantity*1;
+            element.quantity = total;
+            localStorage.setItem("basket", JSON.stringify(basket));
+        }else{
+            alreadyAdded = false;
+        }
+    }
+
+    if(alreadyAdded === true) {
+        return
+    }else{
+        basket.push(order);
+        localStorage.setItem("basket", JSON.stringify(basket));
+    }
+}
